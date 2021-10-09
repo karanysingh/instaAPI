8b9e68c14dfe5d20aaf6174c0619944308e05d12
Karan Yuvraj Singh <56989043+karanysingh@users.noreply.github.com>
2021-10-10 02:39:24 +0530

    env port for hosting

--- main.go
+++ main.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-    "net/http"
-    "log"
-	L "instapi/helper"
 	handlers "instapi/handlers"
+	L "instapi/helper"
+	"net/http"
+	"os"
 )
 
 var w http.ResponseWriter
 var r *http.Request
 
 func main() {
-	
+
 	// CONNECTING TO MONGODB
 	L.ConnectToDB()
 
@@ -23,10 +23,11 @@ func main() {
 	http.HandleFunc("/posts/", handlers.ShowPost)
 	http.HandleFunc("/users/posts/", handlers.ShowAllPosts)
 
-
 	// LISTENER
-    err := http.ListenAndServe(":9090", nil)
-    if err != nil {
-        log.Fatal("ListenAndServe: ", err)
-    }
+	port, err := os.Getenv("PORT")
+	if err != nil {
+		port = "9090"
+	}
+
+	app.Run(":" + port)
 }
