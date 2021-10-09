2883de9fa97645cc05b1d05be26604d0d9ed9b1e
Karan Yuvraj Singh <56989043+karanysingh@users.noreply.github.com>
2021-10-10 00:04:33 +0530

    Add Encyption

--- main.go
+++ main.go
@@ -22,15 +22,7 @@ func main() {
 	http.HandleFunc("/users/", handlers.ShowUser)
 	http.HandleFunc("/posts/", handlers.ShowPost)
 	http.HandleFunc("/users/posts/", handlers.ShowAllPosts)
-	
-	// SCHEMAS
-	// FOLDER STRUCTURE
-	// ERR HANDLING
-	// DATABASE ENV VARIABLES SAFE
-	// VERIFY USING REGEX
-	// PAGINATION
-	// TESTS
-	// FRONTEND
+
 
 	// LISTENER
     err := http.ListenAndServe(":9090", nil)
